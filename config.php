a491e931273507e49fc8d6463f241e2bd9ba5390
roryy <rory@roryy.com>
2014-08-27 22:54:25 +0200

    CLI password support instead of $_GET

--- config.php
+++ config.php
@@ -2,6 +2,6 @@
 //setup password so you only generate when pw is given
 $password = "strifedikzak1337";
 
-if ($password != $_GET['password']) {
+if (!in_array($password,array($_GET['password'],$argv[1]))) {
     die('incorrect password');
 }
