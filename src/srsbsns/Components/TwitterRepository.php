cda74f4c2382344312ed24d667d7ba6405f0eea3
roryy <rory@roryy.com>
2014-08-27 22:31:37 +0200

    twitter begin

---
+++ src/srsbsns/Components/TwitterRepository.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rory
+ * Date: 27-08-14
+ * Time: 21:47
+ */
+
+namespace srsbsns\Components;
+
+
+use srsbsns\Components\Repository\Repository;
+
+class TwitterRepository extends Repository {
+
+    public function save($filePath, $fileName, $contents) {
+        parent::save($filePath, $fileName, $contents);
+
+        // @TODO Twitter logic here
+
+    }
+
+} 
